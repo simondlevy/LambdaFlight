bd4e8c7c1e39f8581027eee84fff7f12084f56d9
simondlevy <simon.d.levy@gmail.com>
2024-02-09 16:16:07 -0500

    modified:   AltitudeHold.hs

--- webots/controllers/haskell/AltitudeHold.hs
+++ webots/controllers/haskell/AltitudeHold.hs
@@ -10,6 +10,8 @@ import Demands
 import State
 import Clock
 
+-- Utils ----------------------------------------------------------------------
+
 smax :: Stream Float -> Stream Float -> Stream Float
 smax x y = if x > y then x else y
 
@@ -23,62 +25,19 @@ rescale value oldmin oldmax newmin newmax =
 
 constrain :: Stream Float -> Stream Float -> Stream Float -> Stream Float
 constrain val min max = if val < min then min else if val > max then max else val
+
+------------------------------------------------------------------------------
  
-
-altitudePid :: Stream Float -> Stream Float -> Stream Float -> Stream Float
-
-altitudePid dt desired measured = output
-
-  where kp = 2.0
-        ki = 0.5
-        integralLimit = 5000
-
-        output_limit = 1.1
-
-        error = desired - measured
-
-        errorIntegral = smax (errorIntegral' + error) integralLimit
-
-        errorIntegral' = [0] ++ errorIntegral
-
-        output = smin (kp * error + ki * errorIntegral) output_limit
-
-climbRatePid dt desired measured = kp * error + ki * errorIntegral
-
-  where kp = 25
-        ki = 15
-        integralLimit = 5000
-
-        error = desired - measured
-
-        errorIntegral = smax (errorIntegral' + error) integralLimit
-
-        errorIntegral' = [0] ++ errorIntegral
-
 runAltitudeHold :: ClockRate -> 
                    Stream Float -> 
                    Stream Float -> 
                    Stream Float -> 
                    Stream Float
 
-runAltitudeHold updateRate thrust z dz = thrust''
-
-  where 
-
-        thrust'' = thrust
-        
-{--
-        -- In hover mode, thrust demand comes in as [-1,+1], so
-        -- we convert it to a target altitude in meters
-        thrust' = rescale thrust (-1) 1 0.2 2.0
- 
-        dt = rateToPeriod updateRate
-
-        climbRate = altitudePid dt thrust' z
-
-        thrust'' = climbRatePid dt climbRate dz
---}
+runAltitudeHold updateRate thrust z dz = thrust
                 
+------------------------------------------------------------------------------
+
 altitudeHold :: Stream Bool ->
                 ClockRate -> 
                 Stream Float -> 
@@ -100,5 +59,3 @@ altitudeHold inHoverMode updateRate thrust z dz = thrust''
         thrust'' = constrain (thrust' * thrust_scale + thrust_base)
                              thrust_min
                              thrust_max
-
- 
