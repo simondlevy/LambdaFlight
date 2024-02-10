21ad86a2e39006187c7113b6e4ad472048819833
simondlevy <simon.d.levy@gmail.com>
2024-02-10 03:11:45 -0500

    modified:   tude.hpp

--- webots/controllers/haskell/tude.hpp
+++ webots/controllers/haskell/tude.hpp
@@ -47,38 +47,24 @@ class AltitudePi {
 
 class ClimbRatePi {
 
-    private:
-
-
-        float _kp;           // proportional gain
-        float _ki;           // integral gain
-        float _dt;           // delta-time dt
-
     public:
-
-        void init(
-                const float kp,
-                const float ki,
-                const float dt,
-                const float samplingRate)
-        {
-            _kp    = kp;
-            _ki    = ki;
-            _dt    = dt;
-        }
 
         float run(const float desired, const float measured)
         {
+            static const float KP = 25;
+            static const float KI = 15;
+            static const float DT = 0.01;
+
             static const float INTEGRATION_LIMIT = 5000;
 
             static float _integ;     
 
             auto error = desired - measured;
 
-            _integ = constrain(_integ + error * _dt, 
+            _integ = constrain(_integ + error * DT, 
                     -INTEGRATION_LIMIT, INTEGRATION_LIMIT);
 
-            return _kp * error + _ki * _integ;
+            return KP * error + KI * _integ;
         }
 
 };
@@ -90,15 +76,11 @@ class AltitudeController : public ClosedLoopController {
         void init(
                 const Clock::rate_t updateRate,
                 const float altitudeKp=2,
-                const float altitudeKi=0.5,
-                const float climbRateKp=25,
-                const float climbRateKi=15)
+                const float altitudeKi=0.5)
         {
             ClosedLoopController::init(updateRate);
 
             _altitudePi.init(altitudeKp, altitudeKi, _dt, _updateRate);
-
-            _climbRatePi.init(climbRateKp, climbRateKi, _dt, _updateRate);
         }
 
         void run(const vehicleState_t & state, demands_t & demands)
