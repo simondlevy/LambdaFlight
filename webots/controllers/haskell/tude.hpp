753d53d45c3e86d542598b8a77c8f196d96ce0c8
simondlevy <simon.d.levy@gmail.com>
2024-02-10 03:26:27 -0500

    modified:   tude.hpp

--- webots/controllers/haskell/tude.hpp
+++ webots/controllers/haskell/tude.hpp
@@ -7,43 +7,24 @@ float constrain(float val, float min, float max)
     return val < min ? min : val > max ? max : val;
 }
 
-class AltitudePi {
+float altitudePid(const float desired, const float measured)
+{
+    static const float KP = 2;
+    static const float KI = 0.5;
+    static const float DT = 0.01;
 
-    private:
+    static const float INTEGRATION_LIMIT = 5000;
 
+    static float _integ;     
 
-        float _kp;           // proportional gain
-        float _ki;           // integral gain
-        float _dt;           // delta-time dt
+    auto error = desired - measured;
 
-    public:
+    _integ = constrain(_integ + error * DT, 
+            -INTEGRATION_LIMIT, INTEGRATION_LIMIT);
 
-        void init(
-                const float kp,
-                const float ki,
-                const float dt,
-                const float samplingRate)
-        {
-            _kp    = kp;
-            _ki    = ki;
-            _dt    = dt;
-        }
+    return KP * error + KI * _integ;
+}
 
-        float run(const float desired, const float measured)
-        {
-            static const float INTEGRATION_LIMIT = 5000;
-
-            static float _integ;     
-
-            auto error = desired - measured;
-
-            _integ = constrain(_integ + error * _dt, 
-                    -INTEGRATION_LIMIT, INTEGRATION_LIMIT);
-
-            return _kp * error + _ki * _integ;
-        }
-
-};
 
 static float climbRatePid(const float desired, const float measured)
 {
@@ -73,20 +54,14 @@ class AltitudeController : public ClosedLoopController {
                 const float altitudeKi=0.5)
         {
             ClosedLoopController::init(updateRate);
-
-            _altitudePi.init(altitudeKp, altitudeKi, _dt, _updateRate);
         }
 
         void run(const vehicleState_t & state, demands_t & demands)
         {
             // Set climb rate based on target altitude
-            auto climbRate = _altitudePi.run(demands.thrust, state.z);
+            auto climbRate = altitudePid(demands.thrust, state.z);
 
             // Set thrust for desired climb rate
             demands.thrust = climbRatePid(climbRate, state.dz);
         }
-
-    private:
-
-        AltitudePi _altitudePi;
 };
