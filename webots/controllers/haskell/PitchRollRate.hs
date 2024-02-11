c423117d08b3c5ff10fdd5d022150d3c2573779c
simondlevy <simon.d.levy@gmail.com>
2024-02-11 15:55:07 -0500

    =More PitchRollRate

--- webots/controllers/haskell/PitchRollRate.hs
+++ webots/controllers/haskell/PitchRollRate.hs
@@ -6,11 +6,40 @@ module PitchRollRate where
 import Language.Copilot
 import Copilot.Compile.C99
 
+runRollRatePid :: Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float
+
+runRollRatePid kp ki kd integralLimit demand rate = 0
+
+------------------------------------------------------------------------------
+
+runPitchRatePid :: Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float -> 
+                  Stream Float
+
+runPitchRatePid kp ki kd integralLimit demand rate = 0
+
+------------------------------------------------------------------------------
+
 runPitchRollRatePid :: (Stream Float, Stream Float) -> (Stream Float, Stream Float) ->
   (Stream Float, Stream Float)
 
 runPitchRollRatePid (rollDemand, pitchDemand) (rollRate, pitchRate) =
   (rollDemand', pitchDemand') 
 
-  where rollDemand' = 0
-        pitchDemand' = 0
+  where kp = 125
+        ki = 250
+        kd = 1.25
+        integral_limit = 33
+
+        rollDemand'  = runRollRatePid  kp ki kd integral_limit rollDemand  rollRate
+        pitchDemand' = runPitchRatePid kp ki kd integral_limit pitchDemand pitchRate
