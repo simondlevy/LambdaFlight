edb71c68102230c32527c58bbdbb4db1fe5796c6
simondlevy <simon.d.levy@gmail.com>
2024-02-10 23:32:58 -0500

    =More altitude hold

--- webots/controllers/haskell/haskell.cpp
+++ webots/controllers/haskell/haskell.cpp
@@ -365,7 +365,7 @@ int main(int argc, char ** argv)
         demands.thrust = demands.thrust * (in_hover_mode ? 1 : THRUST_MAX);
 
         demands.thrust = constrain(demands.thrust * THRUST_SCALE + THRUST_BASE,
-                THRUST_MIN THRUST_MAX);
+                THRUST_MIN, THRUST_MAX);
 
         // Call Haskell Copilot
         step();
