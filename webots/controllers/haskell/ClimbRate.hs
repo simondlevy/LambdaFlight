599481857192dcacfadfd9b118f97b86bb9dc60a
simondlevy <simon.d.levy@gmail.com>
2024-02-13 14:00:07 -0500

    =More ClosedLoopController work

--- webots/controllers/haskell/ClimbRate.hs
+++ webots/controllers/haskell/ClimbRate.hs
@@ -6,6 +6,9 @@ module ClimbRate where
 import Language.Copilot
 import Copilot.Compile.C99
 
+import ClosedLoop
+import Demands
+import State
 import Utils
 
 runClimbRatePid :: SFloat -> SFloat -> SFloat
@@ -25,3 +28,25 @@ runClimbRatePid climbRate dz = kp * error + ki * integ
 
     integ' = [0] ++ integ
 
+------------------------------------------------------------------------------
+
+newRunClimbRatePid :: ClosedLoopController
+
+newRunClimbRatePid inHoverMode state demands = demands'  where
+
+    kp = 25
+    ki = 15
+    dt = 0.01
+
+    integral_limit = 5000
+
+    error = (thrust demands) - (dz state)
+
+    integ = constrain (integ' + error * dt) (-integral_limit) integral_limit
+
+    demands' = Demands (kp * error + ki * integ) 
+                       (roll demands)
+                       (pitch demands)
+                       (yaw demands)
+          
+    integ' = [0] ++ integ
