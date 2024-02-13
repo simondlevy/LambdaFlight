a075c67ee82d4db5f893b7aaa2d6f6b3d2a82b68
simondlevy <simon.d.levy@gmail.com>
2024-02-13 14:59:20 -0500

    =More ClosedLoopController work

--- webots/controllers/haskell/YawRate.hs
+++ webots/controllers/haskell/YawRate.hs
@@ -32,3 +32,31 @@ runYawRatePid desired measured = -(kp * error + ki * integ)
     integ = constrain (integ' + error * dt) (-integral_limit) integral_limit
 
     integ' = [0] ++ integ
+
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+-- Yaw demand is nose-right positive, whereas yaw angle psi and its first
+-- derivative (angular velocity) dspi are nose-right negative.
+-- Hence we negate yaw demand, run the PID closedloop on the
+-- negated demand and the angular velocity, and negate the result to get the
+-- correct yaw demand.
+
+newRunYawRatePid :: SFloat -> SFloat -> SFloat
+newRunYawRatePid desired measured = yaw'
+
+  where
+
+    kp = 120
+    ki = 16.7
+    dt = 0.01
+    integral_limit = 166.7
+
+    error = (-desired) - measured
+
+    integ = constrain (integ' + error * dt) (-integral_limit) integral_limit
+
+    yaw' = -(kp * error + ki * integ)
+
+    integ' = [0] ++ integ
+
