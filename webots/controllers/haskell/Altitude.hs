a668dd60a4fc3cdee2fa764e8f8e005e68126f8d
simondlevy <simon.d.levy@gmail.com>
2024-02-13 13:55:05 -0500

    =More ClosedLoopController work

--- webots/controllers/haskell/Altitude.hs
+++ webots/controllers/haskell/Altitude.hs
@@ -7,6 +7,8 @@ import Language.Copilot
 import Copilot.Compile.C99
 
 import ClosedLoop
+import Demands
+import State
 import Utils
 
 runAltitudePid :: SFloat -> SFloat -> SFloat
@@ -30,5 +32,29 @@ runAltitudePid thrust z = kp * error + ki * integ
    
     integ' = [0] ++ integ
 
+------------------------------------------------------------------------------
 
+newRunAltitudePid :: ClosedLoopController
 
+newRunAltitudePid inHoverMode state demands = demands'  where
+
+    kp = 2
+    ki = 0.5
+    dt = 0.01
+
+    integral_limit = 5000
+
+    -- In hover mode, thrust demand comes in as [-1,+1], so
+    -- we convert it to a target altitude in meters
+    thrust' = rescale (thrust demands) (-1) 1 0.2 2.0
+
+    error = thrust' - (z state)
+    
+    integ = constrain (integ' + error * dt) (-integral_limit) integral_limit
+
+    demands' = Demands (kp * error + ki * integ) 
+                       (roll demands)
+                       (pitch demands)
+                       (yaw demands)
+          
+    integ' = [0] ++ integ
--- webots/controllers/haskell/ClosedLoop.hs
+++ webots/controllers/haskell/ClosedLoop.hs
@@ -5,6 +5,7 @@ import Copilot.Compile.C99
 
 import Demands
 import State
+import Utils
 
-type ClosedLoopController = State -> Demands -> Demands
+type ClosedLoopController = SBool -> State -> Demands -> Demands
 
