b5b1ccc9356ed07d2b40a3e076c8429e9a9c77a7
simondlevy <simon.d.levy@gmail.com>
2024-02-14 20:14:51 -0500

    =More PIController

--- webots/controllers/haskell/Altitude.hs
+++ webots/controllers/haskell/Altitude.hs
@@ -16,18 +16,17 @@ run dt thrust altitude = thrust'' where
 
     kp = 2
     ki = 0.5
-
-    integral_limit = 5000
+    ilimit = 5000
 
     -- In hover mode, thrust demand comes in as [-1,+1], so
     -- we convert it to a target altitude in meters
     thrust' = rescale thrust (-1) 1 0.2 2.0
 
-    error = thrust' - altitude
-    
-    integ = constrain (integ' + error * dt) (-integral_limit) integral_limit
+    (thrust'', integ) = piController kp ki dt ilimit thrust' altitude integ'
 
-    thrust'' = kp * error + ki * integ
+    -- error = thrust' - altitude
+    -- integ = constrain (integ' + error * dt) (-integral_limit) integral_limit
+    -- thrust'' = kp * error + ki * integ
          
     integ' = [0] ++ integ
 
