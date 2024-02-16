2bfa2b18dfe1a7da1071dd9fe2f3ff1f02168794
simondlevy <simon.d.levy@gmail.com>
2024-02-16 17:55:45 -0500

    Cleanup

--- webots/controllers/common.hpp
+++ webots/controllers/common.hpp
@@ -34,10 +34,10 @@ static const float ALTITUDE_TARGET_MAX = 2.0;  // 3.0 in original
 
 static const float DT = .01;
 
-static WbDeviceTag m1_motor;
-static WbDeviceTag m2_motor;
-static WbDeviceTag m3_motor;
-static WbDeviceTag m4_motor;
+static WbDeviceTag _m1_motor;
+static WbDeviceTag _m2_motor;
+static WbDeviceTag _m3_motor;
+static WbDeviceTag _m4_motor;
 
 // These are global so they can be shared with Haskell Copilot ---------------
 
@@ -50,15 +50,15 @@ void step(void);
 void runMotors(float m1, float m2, float m3, float m4)
 {
     // Set simulated motor values
-    wb_motor_set_velocity(m1_motor, +m1);
-    wb_motor_set_velocity(m2_motor, -m2);
-    wb_motor_set_velocity(m3_motor, +m3);
-    wb_motor_set_velocity(m4_motor, -m4);
+    wb_motor_set_velocity(_m1_motor, +m1);
+    wb_motor_set_velocity(_m2_motor, -m2);
+    wb_motor_set_velocity(_m3_motor, +m3);
+    wb_motor_set_velocity(_m4_motor, -m4);
 }
 
 // ---------------------------------------------------------------------------
 
-static WbDeviceTag makeMotor(const char * name, const float direction)
+static WbDeviceTag _makeMotor(const char * name, const float direction)
 {
     auto motor = wb_robot_get_device(name);
 
@@ -73,7 +73,7 @@ static float _rad2deg(const float rad)
     return rad / M_PI * 180;
 }
 
-static void getVehicleState(
+static void _getVehicleState(
         WbDeviceTag & gyro, 
         WbDeviceTag & imu, 
         WbDeviceTag & gps)
@@ -116,7 +116,7 @@ static void getVehicleState(
     zprev = state.z;
 }
 
-static WbDeviceTag makeSensor(
+static WbDeviceTag _makeSensor(
         const char * name, 
         const uint32_t timestep,
         void (*f)(WbDeviceTag tag, int sampling_period))
@@ -126,7 +126,7 @@ static WbDeviceTag makeSensor(
     return sensor;
 }
 
-static float fconstrain(const float val, const float lo, const float hi)
+static float _constrain(const float val, const float lo, const float hi)
 {
     return val < lo ? lo : val > hi ? hi : val;
 }
@@ -138,16 +138,16 @@ static void run(void)
     const int timestep = (int)wb_robot_get_basic_time_step();
 
     // Initialize motors
-    m1_motor = makeMotor("m1_motor", +1);
-    m2_motor = makeMotor("m2_motor", -1);
-    m3_motor = makeMotor("m3_motor", +1);
-    m4_motor = makeMotor("m4_motor", -1);
+    _m1_motor = _makeMotor("_m1_motor", +1);
+    _m2_motor = _makeMotor("_m2_motor", -1);
+    _m3_motor = _makeMotor("_m3_motor", +1);
+    _m4_motor = _makeMotor("_m4_motor", -1);
 
     // Initialize sensors
-    auto imu = makeSensor("inertial_unit", timestep, wb_inertial_unit_enable);
-    auto gyro = makeSensor("gyro", timestep, wb_gyro_enable);
-    auto gps = makeSensor("gps", timestep, wb_gps_enable);
-    auto camera = makeSensor("camera", timestep, wb_camera_enable);
+    auto imu = _makeSensor("inertial_unit", timestep, wb_inertial_unit_enable);
+    auto gyro = _makeSensor("gyro", timestep, wb_gyro_enable);
+    auto gps = _makeSensor("gps", timestep, wb_gps_enable);
+    auto camera = _makeSensor("camera", timestep, wb_camera_enable);
 
     sticksInit();
 
@@ -162,10 +162,10 @@ static void run(void)
         sticksRead(demands);
 
         // Get vehicle state from sensors
-        getVehicleState(gyro, imu, gps);
+        _getVehicleState(gyro, imu, gps);
 
         // Hover mode: integrate stick demand to get altitude target
-        altitudeTarget = fconstrain(
+        altitudeTarget = _constrain(
                 altitudeTarget + demands.thrust * DT, 
                 ALTITUDE_TARGET_MIN, ALTITUDE_TARGET_MAX);
 
