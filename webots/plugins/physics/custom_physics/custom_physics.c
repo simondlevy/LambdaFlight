b0d2d261bb7e1c0bd60427832a5912a37a290071
simondlevy <simon.d.levy@gmail.com>
2024-03-02 21:45:13 -0500

    =Cleanup

--- webots/plugins/physics/custom_physics/custom_physics.c
+++ webots/plugins/physics/custom_physics/custom_physics.c
@@ -1,41 +1,9 @@
 /****************************************************************************
 
-  custom_physics -- A custom physics model for Webots.
+  custom_physics -- A custom physics model for Crazyflie.
 
-  Copyright (C) 2006 Laboratory of Intelligent Systems, EPFL, Lausanne
-Authors:    Alexis Guanella            guanella@ini.phys.ethz.ch
-Antoine Beyeler            antoine.beyeler@epfl.ch
-Jean-Christophe Zufferey   jean-christophe.zufferey@epfl.ch
-Dario Floreano             dario.floreano@epfl.ch
-Web: http://lis.epfl.ch
-
-The authors of any publication arising from research using this software are
-kindly requested to add the following reference:
-
-Zufferey, J.C., Guanella, A., Beyeler, A., Floreano, D. (2006) Flying over
-the Reality Gap: From Simulated to Real Indoor Airships. Autonomous Robots,
-Springer US.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
  ******************************************************************************/
-/*------------------------------------------------------------------------------
-
-Author:		Antoine Beyeler (ab)
-
-------------------------------------------------------------------------------*/
 
 #include <plugins/physics.h>
 
@@ -44,7 +12,7 @@ Author:		Antoine Beyeler (ab)
 #include "utils.h"
 
 // constants
-const char kRobotName[] = "custom_lis";
+const char kRobotName[] = "custom_crazyflie";
 const dReal kRotWebotsToAeroBody[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
 const dReal kRotAeroToWebotsBody[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
 
