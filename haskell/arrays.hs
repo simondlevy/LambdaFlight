507a0744d0af5fdb23e153f49d10ad97a1f94bc6
simondlevy <simon.d.levy@gmail.com>
2024-03-13 02:43:21 -0400

    =More array work

--- haskell/arrays.hs
+++ haskell/arrays.hs
@@ -1,9 +1,8 @@
 import Data.Array
 
-wavefront       :: Array (Int,Int) Float
+wavefront :: Array (Int,Int) Float
 
-wavefront =  a  where
-  a = array ((1,1),(3,3)) [((1,1),1.0),((1,2),1.0),((1,3),1.0),
+wavefront =  array ((1,1),(3,3)) [((1,1),1.0),((1,2),1.0),((1,3),1.0),
                            ((2,1),1.0),((2,2),3.0),((2,3),5.0),
                            ((3,1),1.0),((3,2),5.0),((3,3),13.0)]
 
