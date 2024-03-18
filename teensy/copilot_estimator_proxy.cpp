bcbc1d2a2401cc3d90aa57ba190430599220cb60
simondlevy <simon.d.levy@gmail.com>
2024-03-18 19:57:57 -0400

    modified:   copilot_estimator_proxy.cpp

--- teensy/copilot_estimator_proxy.cpp
+++ teensy/copilot_estimator_proxy.cpp
@@ -19,53 +19,50 @@ static void Madgwick6DOF(
 {
     static const float B_madgwick = 0.04;  
      
-    float recipNorm;
-    float s0, s1, s2, s3;
-    float qDot1, qDot2, qDot3, qDot4;
-    float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
-
     //Convert gyroscope degrees/sec to radians/sec
     gx *= 0.0174533f;
     gy *= 0.0174533f;
     gz *= 0.0174533f;
 
     //Rate of change of quaternion from gyroscope
-    qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
-    qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
-    qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
-    qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+    auto qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
+    auto qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
+    auto qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
+    auto qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
 
     //Compute feedback only if accelerometer measurement valid (avoids NaN in
     //accelerometer normalisation)
     if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+
         //Normalise accelerometer measurement
-        recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+        auto recipNorm = invSqrt(ax * ax + ay * ay + az * az);
         ax *= recipNorm;
         ay *= recipNorm;
         az *= recipNorm;
 
         //Auxiliary variables to avoid repeated arithmetic
-        _2q0 = 2.0f * q0;
-        _2q1 = 2.0f * q1;
-        _2q2 = 2.0f * q2;
-        _2q3 = 2.0f * q3;
-        _4q0 = 4.0f * q0;
-        _4q1 = 4.0f * q1;
-        _4q2 = 4.0f * q2;
-        _8q1 = 8.0f * q1;
-        _8q2 = 8.0f * q2;
-        q0q0 = q0 * q0;
-        q1q1 = q1 * q1;
-        q2q2 = q2 * q2;
-        q3q3 = q3 * q3;
+        auto _2q0 = 2.0f * q0;
+        auto _2q1 = 2.0f * q1;
+        auto _2q2 = 2.0f * q2;
+        auto _2q3 = 2.0f * q3;
+        auto _4q0 = 4.0f * q0;
+        auto _4q1 = 4.0f * q1;
+        auto _4q2 = 4.0f * q2;
+        auto _8q1 = 8.0f * q1;
+        auto _8q2 = 8.0f * q2;
+        auto q0q0 = q0 * q0;
+        auto q1q1 = q1 * q1;
+        auto q2q2 = q2 * q2;
+        auto q3q3 = q3 * q3;
 
         //Gradient decent algorithm corrective step
-        s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-        s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 +
-            _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az; s2 = 4.0f * q0q0 * q2 + _2q0
+        auto s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+        auto s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 +
+            _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az; 
+        auto s2 = 4.0f * q0q0 * q2 + _2q0
             * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 +
             _4q2 * az;
-        s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+        auto s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
         recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); 
         s0 *= recipNorm;
         s1 *= recipNorm;
@@ -86,7 +83,7 @@ static void Madgwick6DOF(
     q3 += qDot4 * invSampleFreq;
 
     //Normalise quaternion
-    recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+    auto recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
     q0 *= recipNorm;
     q1 *= recipNorm;
     q2 *= recipNorm;
