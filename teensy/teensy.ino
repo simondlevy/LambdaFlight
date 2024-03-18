9ef57c3abf41dfdbbaccfa13eff5f9fa72d65900
simondlevy <simon.d.levy@gmail.com>
2024-03-18 19:50:42 -0400

    new file:   copilot_estimator_proxy.cpp

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -30,7 +30,6 @@ static const uint16_t channel_5_failsafe = 2000; //gear, greater than 1500 = thr
 static const uint16_t channel_6_failsafe = 2000; //aux1
 
 //Filter parameters - Defaults tuned for 2kHz loop rate; Do not touch unless you know what you are doing:
-static const float B_madgwick = 0.04;  //Madgwick filter parameter
 static const float B_accel = 0.14;     //Accelerometer LP filter paramter, (MPU6050 default: 0.14. MPU9250 default: 0.2)
 static const float B_gyro = 0.1;       //Gyro LP filter paramter, (MPU6050 default: 0.1. MPU9250 default: 0.17)
 
@@ -59,6 +58,7 @@ bfs::SbusRx sbus(&Serial5);
 // Streams
 float thro_des, roll_des, pitch_des, yaw_des;
 float GyroX, GyroY, GyroZ;
+float AccX, AccY, AccZ;
 float roll_IMU, pitch_IMU, yaw_IMU;
 float dt;
 bool throttle_is_down;
@@ -78,25 +78,21 @@ static uint16_t channel_4_pwm;
 static uint16_t channel_5_pwm;
 static uint16_t channel_6_pwm;
 
-// IMU:
-static float AccX, AccY, AccZ;
-static float AccX_prev, AccY_prev, AccZ_prev;
-static float GyroX_prev, GyroY_prev, GyroZ_prev;
-
-static float q0 = 1.0f; //Initialize quaternion for madgwick filter
-static float q1 = 0.0f;
-static float q2 = 0.0f;
-static float q3 = 0.0f;
-
 // Normalized desired state:
 static float roll_passthru, pitch_passthru, yaw_passthru;
 
-// Mixer
-static float m1_command_scaled, m2_command_scaled, m3_command_scaled, m4_command_scaled;
-static int m1_command_PWM, m2_command_PWM, m3_command_PWM, m4_command_PWM;
+// Motors
+static float m1_command_scaled;
+static float m2_command_scaled;
+static float m3_command_scaled;
+static float m4_command_scaled;
+static int m1_command_PWM;
+static int m2_command_PWM;
+static int m3_command_PWM;
+static int m4_command_PWM;
 
 //Flight status
-static bool armedFly = false;
+static bool armedFly;
 
 static void armedStatus() 
 {
@@ -134,7 +130,10 @@ static void IMUinit()
 
 static void getIMUdata() 
 {
-    int16_t AcX,AcY,AcZ,GyX,GyY,GyZ;
+    static float AccX_prev, AccY_prev, AccZ_prev;
+    static float GyroX_prev, GyroY_prev, GyroZ_prev;
+
+    int16_t AcX = 0, AcY = 0, AcZ = 0, GyX = 0, GyY = 0, GyZ = 0;
 
     mpu6050.getMotion6(&AcX, &AcY, &AcZ, &GyX, &GyY, &GyZ);
 
@@ -163,95 +162,6 @@ static void getIMUdata()
     GyroX_prev = GyroX;
     GyroY_prev = GyroY;
     GyroZ_prev = GyroZ;
-}
-
-static void Madgwick6DOF(
-        float gx, float gy, float gz, float ax, float ay, float az, 
-        float invSampleFreq) 
-{
-    //DESCRIPTION: Attitude estimation through sensor fusion - 6DOF
-    /*
-     * See description of Madgwick() for more information. This is a 6DOF
-     * implimentation for when magnetometer data is not available (for example
-     * when using the recommended MPU6050 IMU for the default setup).
-     */
-    float recipNorm;
-    float s0, s1, s2, s3;
-    float qDot1, qDot2, qDot3, qDot4;
-    float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
-
-    //Convert gyroscope degrees/sec to radians/sec
-    gx *= 0.0174533f;
-    gy *= 0.0174533f;
-    gz *= 0.0174533f;
-
-    //Rate of change of quaternion from gyroscope
-    qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
-    qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
-    qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
-    qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
-
-    //Compute feedback only if accelerometer measurement valid (avoids NaN in
-    //accelerometer normalisation)
-    if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
-        //Normalise accelerometer measurement
-        recipNorm = invSqrt(ax * ax + ay * ay + az * az);
-        ax *= recipNorm;
-        ay *= recipNorm;
-        az *= recipNorm;
-
-        //Auxiliary variables to avoid repeated arithmetic
-        _2q0 = 2.0f * q0;
-        _2q1 = 2.0f * q1;
-        _2q2 = 2.0f * q2;
-        _2q3 = 2.0f * q3;
-        _4q0 = 4.0f * q0;
-        _4q1 = 4.0f * q1;
-        _4q2 = 4.0f * q2;
-        _8q1 = 8.0f * q1;
-        _8q2 = 8.0f * q2;
-        q0q0 = q0 * q0;
-        q1q1 = q1 * q1;
-        q2q2 = q2 * q2;
-        q3q3 = q3 * q3;
-
-        //Gradient decent algorithm corrective step
-        s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-        s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 +
-            _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az; s2 = 4.0f * q0q0 * q2 + _2q0
-            * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 +
-            _4q2 * az;
-        s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
-        recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); 
-        s0 *= recipNorm;
-        s1 *= recipNorm;
-        s2 *= recipNorm;
-        s3 *= recipNorm;
-
-        //Apply feedback step
-        qDot1 -= B_madgwick * s0;
-        qDot2 -= B_madgwick * s1;
-        qDot3 -= B_madgwick * s2;
-        qDot4 -= B_madgwick * s3;
-    }
-
-    //Integrate rate of change of quaternion to yield quaternion
-    q0 += qDot1 * invSampleFreq;
-    q1 += qDot2 * invSampleFreq;
-    q2 += qDot3 * invSampleFreq;
-    q3 += qDot4 * invSampleFreq;
-
-    //Normalise quaternion
-    recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-    q0 *= recipNorm;
-    q1 *= recipNorm;
-    q2 *= recipNorm;
-    q3 *= recipNorm;
-
-    //Compute angles
-    roll_IMU = atan2(q0*q1 + q2*q3, 0.5f - q1*q1 - q2*q2)*57.29577951; //degrees
-    pitch_IMU = -asin(constrain(-2.0f * (q1*q3 - q0*q2),-0.999999,0.999999))*57.29577951; //degrees
-    yaw_IMU = -atan2(q1*q2 + q0*q3, 0.5f - q2*q2 - q3*q3)*57.29577951; //degrees
 }
 
 static void getDesState() 
@@ -580,11 +490,6 @@ void debugLoopRate()
     }
 }
 
-static float invSqrt(float x) 
-{
-    return 1.0/sqrtf(x); 
-}
-
 static void debug(void)
 {
     //Print data at 100hz (uncomment one at a time for troubleshooting) - SELECT ONE:
@@ -592,7 +497,7 @@ static void debug(void)
     //debugDesiredState();  
     //debugGyroData();      
     //debugAccelData();     
-    //debugRollPitchYaw();  
+    debugRollPitchYaw();  
     //debugMotorCommands(); 
     //debugServoCommands(); 
     //debugLoopRate();      
@@ -655,14 +560,16 @@ void loop()
     // Get vehicle state
     getIMUdata(); 
 
-    Madgwick6DOF(GyroX, -GyroY, -GyroZ, -AccX, AccY, AccZ, dt); 
+    // Copilot state estimator
+    void copilot_step_estimator(void);
+    copilot_step_estimator(); 
 
     // Compute desired state
     getDesState(); 
 
-    // Copilot
-    void step(void);
-    step(); 
+    // Copilot core
+    void copilot_step_core(void);
+    copilot_step_core(); 
 
     scaleCommands(); 
 
@@ -683,7 +590,8 @@ void loop()
     loopRate(2000); //Do not exceed 2000Hz, all filter paras tuned to 2000Hz by default
 }
 
-// Called by Copilot
+// Called by Copilot ---------------------------------------------------------
+
 void setMotors(const float m1, const float m2, const float m3, const float m4)
 
 {
@@ -693,4 +601,9 @@ void setMotors(const float m1, const float m2, const float m3, const float m4)
     m4_command_scaled = m4;
 }
 
-
+void setState(const float phi, const float theta, const float psi)
+{
+    roll_IMU = phi;
+    pitch_IMU = theta;
+    yaw_IMU = psi;
+}
