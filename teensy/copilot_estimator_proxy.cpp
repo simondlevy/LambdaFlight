e1b4f3694738cec7bead78934bd91d03f24ec5d4
simondlevy <simon.d.levy@gmail.com>
2024-03-18 20:07:36 -0400

    modified:   copilot_estimator_proxy.cpp

--- teensy/copilot_estimator_proxy.cpp
+++ teensy/copilot_estimator_proxy.cpp
@@ -14,66 +14,67 @@ static float q2 = 0.0f;
 static float q3 = 0.0f;
 
 static void Madgwick6DOF(
-        float gx, float gy, float gz, float ax, float ay, float az, 
-        float invSampleFreq) 
+        const float gx, 
+        const float gy, 
+        const float gz, 
+        const float ax, 
+        const float ay, 
+        const float az, 
+        const float invSampleFreq) 
 {
     static const float B_madgwick = 0.04;  
      
     //Convert gyroscope degrees/sec to radians/sec
-    gx *= 0.0174533f;
-    gy *= 0.0174533f;
-    gz *= 0.0174533f;
+    const auto ggx = gx * 0.0174533f;
+    const auto ggy = gy * 0.0174533f;
+    const auto ggz = gz * 0.0174533f;
 
     //Rate of change of quaternion from gyroscope
-    auto qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
-    auto qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
-    auto qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
-    auto qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
+    auto qDot1 = 0.5f * (-q1 * ggx - q2 * ggy - q3 * ggz);
+    auto qDot2 = 0.5f * (q0 * ggx + q2 * ggz - q3 * ggy);
+    auto qDot3 = 0.5f * (q0 * ggy - q1 * ggz + q3 * ggx);
+    auto qDot4 = 0.5f * (q0 * ggz + q1 * ggy - q2 * ggx);
 
     //Compute feedback only if accelerometer measurement valid (avoids NaN in
     //accelerometer normalisation)
-    if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+    if (!((ax == 0) && (ay == 0) && (az == 0))) {
 
         //Normalise accelerometer measurement
-        auto recipNorm = invSqrt(ax * ax + ay * ay + az * az);
-        ax *= recipNorm;
-        ay *= recipNorm;
-        az *= recipNorm;
+        const auto recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+        auto aax = ax * recipNorm;
+        auto aay = ay * recipNorm;
+        auto aaz = az * recipNorm;
 
         //Auxiliary variables to avoid repeated arithmetic
-        auto _2q0 = 2.0f * q0;
-        auto _2q1 = 2.0f * q1;
-        auto _2q2 = 2.0f * q2;
-        auto _2q3 = 2.0f * q3;
-        auto _4q0 = 4.0f * q0;
-        auto _4q1 = 4.0f * q1;
-        auto _4q2 = 4.0f * q2;
-        auto _8q1 = 8.0f * q1;
-        auto _8q2 = 8.0f * q2;
-        auto q0q0 = q0 * q0;
-        auto q1q1 = q1 * q1;
-        auto q2q2 = q2 * q2;
-        auto q3q3 = q3 * q3;
+        const auto _2q0 = 2 * q0;
+        const auto _2q1 = 2 * q1;
+        const auto _2q2 = 2 * q2;
+        const auto _2q3 = 2 * q3;
+        const auto _4q0 = 4 * q0;
+        const auto _4q1 = 4 * q1;
+        const auto _4q2 = 4 * q2;
+        const auto _8q1 = 8 * q1;
+        const auto _8q2 = 8 * q2;
+        const auto q0q0 = q0 * q0;
+        const auto q1q1 = q1 * q1;
+        const auto q2q2 = q2 * q2;
+        const auto q3q3 = q3 * q3;
 
         //Gradient decent algorithm corrective step
-        auto s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-        auto s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 +
-            _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az; 
-        auto s2 = 4.0f * q0q0 * q2 + _2q0
-            * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 +
-            _4q2 * az;
-        auto s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
-        recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); 
-        s0 *= recipNorm;
-        s1 *= recipNorm;
-        s2 *= recipNorm;
-        s3 *= recipNorm;
+        const auto s0 = _4q0 * q2q2 + _2q2 * aax + _4q0 * q1q1 - _2q1 * aay;
+        const auto s1 = _4q1 * q3q3 - _2q3 * aax + 4.0f * q0q0 * q1 - _2q0 * aay - _4q1 +
+            _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * aaz; 
+        const auto s2 = 4.0f * q0q0 * q2 + _2q0
+            * aax + _4q2 * q3q3 - _2q3 * aay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 +
+            _4q2 * aaz;
+        const auto s3 = 4.0f * q1q1 * q3 - _2q1 * aax + 4.0f * q2q2 * q3 - _2q2 * aay;
+        const auto rrecipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); 
 
         //Apply feedback step
-        qDot1 -= B_madgwick * s0;
-        qDot2 -= B_madgwick * s1;
-        qDot3 -= B_madgwick * s2;
-        qDot4 -= B_madgwick * s3;
+        qDot1 -= B_madgwick * s0 * rrecipNorm;
+        qDot2 -= B_madgwick * s1 * rrecipNorm;
+        qDot3 -= B_madgwick * s2 * rrecipNorm;
+        qDot4 -= B_madgwick * s3 * rrecipNorm;
     }
 
     //Integrate rate of change of quaternion to yield quaternion
@@ -83,18 +84,18 @@ static void Madgwick6DOF(
     q3 += qDot4 * invSampleFreq;
 
     //Normalise quaternion
-    auto recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+    const auto recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
     q0 *= recipNorm;
     q1 *= recipNorm;
     q2 *= recipNorm;
     q3 *= recipNorm;
 
     //Compute angles in degrees
-    auto phi = 
+    const auto phi = 
         atan2(q0*q1 + q2*q3, 0.5f - q1*q1 - q2*q2)*57.29577951; 
-    auto theta = 
+    const auto theta = 
         -asin(constrain(-2.0f * (q1*q3 - q0*q2),-0.999999,0.999999))*57.29577951;
-    auto psi = 
+    const auto psi = 
         -atan2(q1*q2 + q0*q3, 0.5f - q2*q2 - q3*q3)*57.29577951;
 
     void setState(const float phi, const float theta, const float psi);
