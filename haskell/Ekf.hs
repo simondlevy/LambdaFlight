95e79ab31c8662afaa70f8cf3c5b96290fdb594d
simondlevy <simon.d.levy@gmail.com>
2024-03-31 15:09:36 -0400

    modified:   Ekf.hs

--- haskell/Ekf.hs
+++ haskell/Ekf.hs
@@ -1,5 +1,5 @@
 {--
-  EKF algorithm for Crazyflie
+  EKF algorithm
  
   Copyright (C) 2024 Simon D. Levy
  
@@ -8,7 +8,7 @@
   the Free Software Foundation, in version 3.
  
   This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  but WITHOUT ANY WARRANTY without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
  
@@ -16,15 +16,22 @@
   along with this program. If not, see <http://www.gnu.org/licenses/>.
 --} 
 
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE RebindableSyntax #-}
 
 module Main where
 
-import Language.Copilot
+import Language.Copilot hiding(atan2)
 import Copilot.Compile.C99
 
 import Utils
+
+-- Initial variances, uncertain of position, but know we're stationary and
+-- roughly flat
+stdev_initial_position_z          = 1.0  :: Float
+stdev_initial_velocity            = 0.01 :: Float
+stdev_initial_attitude_roll_pitch = 0.01 :: Float
+stdev_initial_attitude_yaw        = 0.01 :: Float
 
 ------------------------------------------------------------------------------
 
@@ -36,220 +43,144 @@ ekfMode = extern "stream_ekfMode" Nothing
 nowMsec :: SInt32
 nowMsec = extern "stream_nowMsec" Nothing
 
-mode_init      = 0 :: EkfMode
-mode_predict   = 1 :: EkfMode
-mode_update    = 2 :: EkfMode
-mode_finalize  = 3 :: EkfMode
-mode_get_state = 4 :: EkfMode
+mode_init              = 0 :: EkfMode
+mode_predict           = 1 :: EkfMode
+mode_finalize          = 2 :: EkfMode
+mode_get_state         = 3 :: EkfMode
+mode_update_with_gyro  = 4 :: EkfMode
+mode_update_with_accel = 5 :: EkfMode
+mode_update_with_range = 6 :: EkfMode
+mode_update_with_flow  = 7 :: EkfMode
 
 ------------------------------------------------------------------------------
 
-data Axis3f = Axis3f {
+sqr :: Float -> Float
+sqr x = x * x
 
-    x :: SFloat
-  , y :: SFloat
-  , z :: SFloat
+z = sqr stdev_initial_position_z 
+d = sqr stdev_initial_velocity 
+e = sqr stdev_initial_attitude_roll_pitch
+y = sqr stdev_initial_attitude_yaw
 
-}
+type EkfArray = Array 7 (Array 7 Float)
 
-initAxis3f :: SBool -> Axis3f -> Axis3f
+raw_pinit :: EkfArray
 
-initAxis3f init axis3f = Axis3f x' y' z' where
+raw_pinit =  array [--  z   dx  dy  dz  e0  e1  e2
+                 array [z,  0,  0,  0,  0,  0,  0], -- z
+                 array [0,  d,  0,  0,  0,  0,  0], -- dx
+                 array [0,  0,  d,  0,  0,  0,  0], -- dy
+                 array [0,  0,  0,  d,  0,  0,  0], -- dz
+                 array [0,  0,  0,  0,  e,  0,  0], -- e0
+                 array [0,  0,  0,  0,  0,  e,  0], -- e1
+                 array [0,  0,  0,  0,  0,  0,  y]  -- e2
+             ] 
 
-  x' = if init then 0 else x axis3f
-  y' = if init then 0 else y axis3f
-  z' = if init then 0 else z axis3f
-  
-------------------------------------------------------------------------------
+type SEkfArray = Stream EkfArray
 
-data Axis3fSubSampler = Axis3fSubSampler {
+pinit :: SEkfArray
 
-     summ :: Axis3f
-   , count :: SInt32
-   , conversionFactor :: SFloat
-   , subSample :: Axis3f
-}
+pinit = [ raw_pinit ] ++ pinit
 
-initAxis3fSubSampler :: SBool -> Axis3fSubSampler -> Axis3fSubSampler
+init :: (SEkfArray, (SFloat, SFloat, SFloat, SFloat))
 
-initAxis3fSubSampler init a3fss = a3fss' where
-
-  a3fss' = Axis3fSubSampler summ' count' conversionFactor' subSample'
-
-  summ' = initAxis3f init (summ a3fss)
-  count' = if init then 0 else count a3fss
-  conversionFactor' = if init then 0 else conversionFactor a3fss
-  subSample' = initAxis3f init (subSample a3fss)
+init = (pinit, (1, 0, 0, 0))
 
 ------------------------------------------------------------------------------
 
-data Quat = Quat { 
-    qw :: SFloat
-  , qx :: SFloat
-  , qy :: SFloat
-  , qz :: SFloat 
-}
+step :: (SFloat, SFloat, SFloat, SFloat, SFloat, SFloat) 
 
-initQuat :: SBool -> Quat -> Quat
+step = (dx, dy, dz, phi, theta, psi) where
 
-initQuat init quat = Quat qw' qx' qy' qz' where
+   init = ekfMode == mode_init
 
-  qw' = if init then 1 else qw quat
-  qx' = if init then 0 else qx quat
-  qy' = if init then 0 else qy quat
-  qz' = if init then 0 else qz quat
+   gyroSubsamplerX = (if init then 0 else gyroSubsamplerX') :: SFloat
+   gyroSubsamplerY = (if init then 0 else gyroSubsamplerY') :: SFloat
+   gyroSubsamplerZ = (if init then 0 else gyroSubsamplerZ') :: SFloat
 
-------------------------------------------------------------------------------
+   accelSubsamplerX = (if init then 0 else accelSubsamplerX') :: SFloat
+   accelSubsamplerY = (if init then 0 else accelSubsamplerY') :: SFloat
+   accelSubsamplerZ = (if init then 0 else accelSubsamplerZ') :: SFloat
 
-data EkfState = EkfState {
+   pmat = if init then pinit else pmat'
 
-    zz :: SFloat
-  , dx :: SFloat
-  , dy :: SFloat
-  , dz :: SFloat
-  , e0 :: SFloat
-  , e1 :: SFloat
-  , e2 :: SFloat
-}
+   dx = if init then 0 else dx'
 
-initEkfState :: SBool -> EkfState -> EkfState
+   dy = if init then 0 else dy'
 
-initEkfState init ekfState = EkfState zz' dx' dy' dz' e0' e1' e2' where
+   dz = if init then 0 else r20 * dx' + r21 * dy' + r22 * dz'
 
-  zz' = if init then 0 else zz ekfState
-  dx' = if init then 0 else dx ekfState
-  dy' = if init then 0 else dy ekfState
-  dz' = if init then 0 else dz ekfState
-  e0' = if init then 0 else e0 ekfState
-  e1' = if init then 0 else e1 ekfState
-  e2' = if init then 0 else e2 ekfState
+   z = (if init then 0 else z') :: SFloat
 
-------------------------------------------------------------------------------
+   qw = if init then 1 else qw'
+   qx = if init then 0 else qx'
+   qy = if init then 0 else qy'
+   qz = if init then 0 else qz'
 
- 
-data Ekf = Ekf { 
+   -- Set the initial rotation matrix to the identity. This only affects  the
+   -- first prediction step, since in the finalization, after shifting 
+   -- attitude errors into the attitude state, the rotation matrix is updated.
+   r20 = if init then 0 else r20'
+   r21 = if init then 0 else r21'
+   r22 = if init then 1 else r22'
 
-    ekfState :: EkfState
+   isUpdated = if init then false else isUpdated'
 
-  , quat :: Quat
+   lastPredictionMsec = (if init then nowMsec else lastPredictionMsec') :: SInt32
 
-  , accSubSampler :: Axis3fSubSampler
-  , gyroSubSampler :: Axis3fSubSampler
+   lastProcessNoiseUpdateMsec = 
+     (if init then nowMsec else lastProcessNoiseUpdateMsec') :: SInt32
 
-  , lastPredictionMsec :: SInt32
-  , lastProcessNoiesUpdateMsec :: SInt32
-  , isUpdated :: SBool
+   phi = rad2deg $ atan2 (2 * (qy*qz + qw*qx)) (qw*qw - qx*qx - qy*qy + qz*qz)
 
-  -- third row (Z) of attitude as a rotation matrix for prediction
-  , r20 :: SFloat
-  , r21 :: SFloat
-  , r22 :: SFloat
-}
+   -- Negate for ENU
+   theta = -(rad2deg $ asin ((-2) * (qx*qz - qw*qy)))
 
+   psi = rad2deg $ atan2 (2 * (qx*qy + qw*qz)) (qw*qw + qx*qx - qy*qy - qz*qz)
 
-runEkf :: Ekf
+   pmat' = [raw_pinit] ++ pmat
 
-runEkf = Ekf ekfState quat
-                      accSubSampler
-                      gyroSubSampler
-                      lastPredictionMsec 
-                      lastProcessNoiseUpdateMsec 
-                      isUpdated 
-                      r20 
-                      r21 
-                      r22
+   -- EKF state
+   dx' = [0] ++ dx
+   dy' = [0] ++ dy
+   dz' = [0] ++ dz
+   z' = [0] ++ z
 
-  where
+   -- Quaternion
+   qw' = [0] ++ qw
+   qx' = [0] ++ qx
+   qy' = [0] ++ qy
+   qz' = [0] ++ qz
 
-    init = ekfMode == mode_init
+   -- Rotation vector
+   r20' = [0] ++ r20
+   r21' = [0] ++ r21
+   r22' = [1] ++ r22
 
-    ekfState = initEkfState init (EkfState zz' dx' dy' dz' e0' e1' e2')
+   -- Gyro subsampler
+   gyroSubsamplerX' = [0] ++ gyroSubsamplerX 
+   gyroSubsamplerY' = [0] ++ gyroSubsamplerY 
+   gyroSubsamplerZ' = [0] ++ gyroSubsamplerZ 
 
-    quat = initQuat init (Quat qw' qx' qy' qz')
+   -- Accel subsampler
+   accelSubsamplerX' = [0] ++ accelSubsamplerX 
+   accelSubsamplerY' = [0] ++ accelSubsamplerY 
+   accelSubsamplerZ' = [0] ++ accelSubsamplerZ 
 
-    accSubSampler = 
-      initAxis3fSubSampler init 
-                       (Axis3fSubSampler accSumm'
-                                     accCount' 
-                                     accConversionFactor'
-                                     accSubSampler')
-    gyroSubSampler = 
-      initAxis3fSubSampler init 
-                       (Axis3fSubSampler gyroSumm'
-                                     gyroCount' 
-                                     gyroConversionFactor'
-                                     gyroSubSampler')
-
-    isUpdated = if init then false else isUpdated'
-
-    lastPredictionMsec = if init then nowMsec else lastPredictionMsec'
-
-    lastProcessNoiseUpdateMsec = if init then nowMsec 
-                            else lastProcessNoiseUpdateMsec'
-
-    accSumm' = Axis3f asx' asy' asz'
-
-    accSubSampler' = Axis3f ax' ay' az'
-
-    gyroSumm' = Axis3f gsx' gsy' gsz'
-
-    gyroSubSampler' = Axis3f gx' gy' gz'
-
-    r20 = if init then 0 else r20'
-    r21 = if init then 0 else r20'
-    r22 = if init then 1 else r20'
-
-    ax' = [0] ++ (x accSubSampler')
-    ay' = [0] ++ (y accSubSampler')
-    az' = [0] ++ (z accSubSampler')
-
-    asx' = [0] ++ (x accSumm')
-    asy' = [0] ++ (y accSumm')
-    asz' = [0] ++ (z accSumm')
-
-    gx' = [0] ++ (x gyroSubSampler')
-    gy' = [0] ++ (y gyroSubSampler')
-    gz' = [0] ++ (z gyroSubSampler')
-
-    gsx' = [0] ++ (x gyroSumm')
-    gsy' = [0] ++ (y gyroSumm')
-    gsz' = [0] ++ (z gyroSumm')
-
-    qw' = [1] ++ (qw quat)
-    qx' = [0] ++ (qx quat)
-    qy' = [0] ++ (qy quat)
-    qz' = [0] ++ (qz quat)
-
-    zz' = [0] ++ (zz ekfState)
-    dx' = [0] ++ (dx ekfState)
-    dy' = [0] ++ (dy ekfState)
-    dz' = [0] ++ (dz ekfState)
-    e0' = [0] ++ (e0 ekfState)
-    e1' = [0] ++ (e1 ekfState)
-    e2' = [0] ++ (e2 ekfState)
-
-    accCount' = [0] ++ (count accSubSampler)
-    accConversionFactor' = [0] ++ (conversionFactor accSubSampler)
-
-    gyroCount' = [0] ++ (count gyroSubSampler)
-    gyroConversionFactor' = [0] ++ (conversionFactor gyroSubSampler)
-
-    lastPredictionMsec' = [0] ++ lastPredictionMsec
-
-    lastProcessNoiseUpdateMsec' = [0] ++ lastProcessNoiseUpdateMsec
-
-    isUpdated' = [False] ++ isUpdated
-
-    r20' = [0] ++ r20
-    r21' = [0] ++ r21
-    r22' = [1] ++ r22
+   isUpdated' = [False] ++ isUpdated
+   lastPredictionMsec' = [0] ++ lastPredictionMsec
+   lastProcessNoiseUpdateMsec' = [0] ++ lastProcessNoiseUpdateMsec
 
 ------------------------------------------------------------------------------
 
 spec = do
 
-    trigger "dummy" true [ ]
+  let (dx, dy, dz, phi, theta, psi) = step
 
--- Compile the spec
+  trigger "setState" 
+           (ekfMode == mode_get_state) 
+           [arg dx, arg dy, arg dz, arg phi, arg theta, arg psi]
+
 main = reify spec >>= 
-  compileWith (CSettings "copilot_step_ekf" ".") "copilot_ekf"
+
+  compileWith (CSettings "foo" ".") "foo"
