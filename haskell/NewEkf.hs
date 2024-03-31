6796b6e376d371fd530e186884d9e45ca96e6cd7
simondlevy <simon.d.levy@gmail.com>
2024-03-31 04:05:19 -0400

    =More Haskell EKF

--- haskell/NewEkf.hs
+++ haskell/NewEkf.hs
@@ -8,7 +8,7 @@
   the Free Software Foundation, in version 3.
  
   This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  but WITHOUT ANY WARRANTY without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
  
@@ -43,11 +43,14 @@ ekfMode = extern "stream_ekfMode" Nothing
 nowMsec :: SInt32
 nowMsec = extern "stream_nowMsec" Nothing
 
-mode_init      = 0 :: EkfMode
-mode_predict   = 1 :: EkfMode
-mode_update    = 7 :: EkfMode
-mode_finalize  = 3 :: EkfMode
-mode_get_state = 4 :: EkfMode
+mode_init              = 0 :: EkfMode
+mode_predict           = 1 :: EkfMode
+mode_finalize          = 2 :: EkfMode
+mode_get_state         = 3 :: EkfMode
+mode_update_with_gyro  = 4 :: EkfMode
+mode_update_with_accel = 5 :: EkfMode
+mode_update_with_range = 6 :: EkfMode
+mode_update_with_flow  = 7 :: EkfMode
 
 ------------------------------------------------------------------------------
 
@@ -91,6 +94,14 @@ step = (dx, dy, dz, phi, theta, psi) where
 
    init = ekfMode == mode_init
 
+   gyroSubsamplerX = (if init then 0 else gyroSubsamplerX') :: SFloat
+   gyroSubsamplerY = (if init then 0 else gyroSubsamplerY') :: SFloat
+   gyroSubsamplerZ = (if init then 0 else gyroSubsamplerZ') :: SFloat
+
+   accelSubsamplerX = (if init then 0 else accelSubsamplerX') :: SFloat
+   accelSubsamplerY = (if init then 0 else accelSubsamplerY') :: SFloat
+   accelSubsamplerZ = (if init then 0 else accelSubsamplerZ') :: SFloat
+
    pmat = if init then pinit else pmat'
 
    dx = if init then 0 else dx'
@@ -99,6 +110,27 @@ step = (dx, dy, dz, phi, theta, psi) where
 
    dz = if init then 0 else r20 * dx' + r21 * dy' + r22 * dz'
 
+   z = (if init then 0 else z') :: SFloat
+
+   qw = if init then 1 else qw'
+   qx = if init then 0 else qx'
+   qy = if init then 0 else qy'
+   qz = if init then 0 else qz'
+
+   -- Set the initial rotation matrix to the identity. This only affects  the
+   -- first prediction step, since in the finalization, after shifting 
+   -- attitude errors into the attitude state, the rotation matrix is updated.
+   r20 = if init then 0 else r20'
+   r21 = if init then 0 else r21'
+   r22 = if init then 1 else r22'
+
+   isUpdated = if init then false else isUpdated'
+
+   lastPredictionMsec = (if init then nowMsec else lastPredictionMsec') :: SInt32
+
+   lastProcessNoiseUpdateMsec = 
+     (if init then nowMsec else lastProcessNoiseUpdateMsec') :: SInt32
+
    phi = rad2deg $ atan2 (2 * (qy*qz + qw*qx)) (qw*qw - qx*qx - qy*qy + qz*qz)
 
    -- Negate for ENU
@@ -106,20 +138,38 @@ step = (dx, dy, dz, phi, theta, psi) where
 
    psi = rad2deg $ atan2 (2 * (qx*qy + qw*qz)) (qw*qw + qx*qx - qy*qy - qz*qz)
 
-   qw = 0
-   qx = 0
-   qy = 0
-   qz = 0
-
-   r20 = 0
-   r21 = 0
-   r22 = 0
-
    pmat' = [raw_pinit] ++ pmat
+
+   -- EKF state
    dx' = [0] ++ dx
    dy' = [0] ++ dy
    dz' = [0] ++ dz
+   z' = [0] ++ z
 
+   -- Quaternion
+   qw' = [0] ++ qw
+   qx' = [0] ++ qx
+   qy' = [0] ++ qy
+   qz' = [0] ++ qz
+
+   -- Rotation vector
+   r20' = [0] ++ r20
+   r21' = [0] ++ r21
+   r22' = [1] ++ r22
+
+   -- Gyro subsampler
+   gyroSubsamplerX' = [0] ++ gyroSubsamplerX 
+   gyroSubsamplerY' = [0] ++ gyroSubsamplerY 
+   gyroSubsamplerZ' = [0] ++ gyroSubsamplerZ 
+
+   -- Accel subsampler
+   accelSubsamplerX' = [0] ++ accelSubsamplerX 
+   accelSubsamplerY' = [0] ++ accelSubsamplerY 
+   accelSubsamplerZ' = [0] ++ accelSubsamplerZ 
+
+   isUpdated' = [False] ++ isUpdated
+   lastPredictionMsec' = [0] ++ lastPredictionMsec
+   lastProcessNoiseUpdateMsec' = [0] ++ lastProcessNoiseUpdateMsec
 
 ------------------------------------------------------------------------------
 
