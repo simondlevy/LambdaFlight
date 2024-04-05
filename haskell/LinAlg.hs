d4a6e21d4e9c106d2d70da9ed01ebbe758377bb7
simondlevy <simon.d.levy@gmail.com>
2024-04-05 13:48:07 -0400

    =More Ekf.hs

--- haskell/LinAlg.hs
+++ haskell/LinAlg.hs
@@ -7,6 +7,14 @@ type Matrix = [[Float]]
 msq :: Matrix -> Matrix
 msq a = a !*! a
 
+printmat :: Matrix -> IO()
+printmat b = do
+    print $ b!!0
+    print $ b!!1
+    print $ b!!2
+    print $ b!!3
+    print $ b!!4
+
 main = do
 
     let a = [ [ 0,1,2,3,4 ],
@@ -17,9 +25,4 @@ main = do
 
     let b = msq a 
 
-    print $ b!!0
-    print $ b!!1
-    print $ b!!2
-    print $ b!!3
-    print $ b!!4
-
+    printmat b
