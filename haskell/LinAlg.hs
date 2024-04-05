05a6695c56ddc73d675a9c670173022b89df37c8
simondlevy <simon.d.levy@gmail.com>
2024-04-05 14:12:45 -0400

    =More Ekf.hs

--- haskell/LinAlg.hs
+++ haskell/LinAlg.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Linear.Matrix
+import Linear.Matrix hiding(transpose)
+import Data.List
 
 type Vector = [Float]
 
@@ -8,7 +9,6 @@ type Matrix = [Vector]
 
 msq :: Matrix -> Matrix
 msq a = a !*! a
-
 
 printvec' :: Vector -> Int -> IO()
 printvec' _ 0 = do
@@ -35,12 +35,18 @@ printmat :: Matrix -> IO()
 printmat a = do
     printmat' a (length a)
 
+-- https://stackoverflow.com/questions/34423279/rotate-a-matrix-in-haskell
+transp :: Matrix -> Matrix
+transp = transpose . map reverse
+
 main = do
 
-    let a = [ [ 0,1,2,3,4 ],
-              [ 1,2,3,4,0 ],
-              [ 2,3,4,0,1 ],
-              [ 3,4,0,1,2 ],
-              [ 4,0,1,2,3 ] ]
+    let a = [ [ 0,1,2],
+              [ 3,4,5],
+              [ 6,7,8 ] ]
 
-    printmat $ msq a
+    let b = transpose a
+
+    printmat $ a
+    print "---------------------"
+    printmat $ b
