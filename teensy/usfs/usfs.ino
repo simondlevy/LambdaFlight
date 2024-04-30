af06df55c50b7e689e1ecfc0ab43457c18c9813d
simondlevy <simon.d.levy@gmail.com>
2024-04-30 01:52:45 -0400

    =More usfs.ino

--- teensy/usfs/usfs.ino
+++ teensy/usfs/usfs.ino
@@ -192,6 +192,29 @@ static void readReceiver()
     }
 }
 
+static void maintainLoopRate(const uint32_t current_time) 
+{
+    //DESCRIPTION: Regulate main loop rate to specified frequency in Hz
+    /*
+     * It's good to operate at a constant loop rate for filters to remain
+     * stable and whatnot. Interrupt routines running in the background cause
+     * the loop rate to fluctuate. This function basically just waits at the
+     * end of every loop iteration until the correct time has passed since the
+     * start of the current loop for the desired loop rate in Hz. 2kHz is a
+     * good rate to be at because the loop nominally will run between 2.8kHz -
+     * 4.2kHz. This lets us have a little room to add extra computations and
+     * remain above 2kHz, without needing to retune all of our filtering
+     * parameters.
+     */
+    float invFreq = 1.0/LOOP_RATE*1000000.0;
+    auto checker = micros();
+
+    //Sit in loop until appropriate time has passed
+    while (invFreq > (checker - current_time)) {
+        checker = micros();
+    }
+}
+
 
 static void ekfStep(void)
 {
@@ -317,5 +340,7 @@ void loop()
     ekfStep();
 
     readReceiver();
+
+    maintainLoopRate(_current_time); 
 
 }  // loop
