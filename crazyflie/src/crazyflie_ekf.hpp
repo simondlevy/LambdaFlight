cf43c80cf10bebef86fb5d96e6f57366d359bc44
simondlevy <simon.d.levy@gmail.com>
2024-05-08 22:49:30 -0400

    =More Ekf superclass

--- crazyflie/src/crazyflie_ekf.hpp
+++ crazyflie/src/crazyflie_ekf.hpp
@@ -2,21 +2,11 @@
 
 #include <string.h>
 
-class CrazyflieEkf {
+#include "ekf.hpp"
+
+class CrazyflieEkf : public Ekf {
 
     private:
-
-        typedef struct {
-
-            float dat[EKF_N];
-
-        } vector_t;
-
-        typedef struct {
-
-            float dat[EKF_N][EKF_N];
-
-        } matrix_t;
 
         typedef struct {
 
@@ -725,113 +715,6 @@ class CrazyflieEkf {
 
             memcpy(&A.dat, a, sizeof(A));
         } 
-
-        static void makemat(const float dat[EKF_N][EKF_N], matrix_t & a)
-        {
-            for (uint8_t i=0; i<EKF_N; ++i) {
-                for (uint8_t j=0; j<EKF_N; ++j) {
-                    a.dat[i][j] = dat[i][j];
-                }
-            }
-        }
-
-        static void transpose(const matrix_t & a, matrix_t & at)
-        {
-            for (uint8_t i=0; i<EKF_N; ++i) {
-                for (uint8_t j=0; j<EKF_N; ++j) {
-                    auto tmp = a.dat[i][j];
-                    at.dat[i][j] = a.dat[j][i];
-                    at.dat[j][i] = tmp;
-                }
-            }
-        }
-
-        static float dot(const vector_t & x, const vector_t & y) 
-        {
-            float d = 0;
-
-            for (uint8_t k=0; k<EKF_N; k++) {
-                d += x.dat[k] * y.dat[k];
-            }
-
-            return d;
-        }
-
-        static float get(const matrix_t & a, const uint8_t i, const uint8_t j)
-        {
-            return a.dat[i][j];
-        }
-
-        static float get(const vector_t & x, const uint8_t i)
-        {
-            return x.dat[i];
-        }
-
-        static void set(vector_t & x, const uint8_t i, const float val)
-        {
-            x.dat[i] = val;
-        }
-
-        static void set(matrix_t & a, const uint8_t i, const uint8_t j, const float val)
-        {
-            a.dat[i][j] = val;
-        }
-
-        static float dot(
-                const matrix_t & a, 
-                const matrix_t & b, 
-                const uint8_t i, 
-                const uint8_t j)
-        {
-            float d = 0;
-
-            for (uint8_t k=0; k<EKF_N; k++) {
-                d += a.dat[i][k] * b.dat[k][j];
-            }
-
-            return d;
-        }
-
-        // Matrix * Matrix
-        static void multiply( const matrix_t a, const matrix_t b, matrix_t & c)
-        {
-            for (uint8_t i=0; i<EKF_N; i++) {
-
-                for (uint8_t j=0; j<EKF_N; j++) {
-
-                    c.dat[i][j] = dot(a, b, i, j);
-                }
-            }
-        }
-
-        // Matrix * Vector
-        static void multiply(const matrix_t & a, const vector_t & x, vector_t & y)
-        {
-            for (uint8_t i=0; i<EKF_N; i++) {
-                y.dat[i] = 0;
-                for (uint8_t j=0; j<EKF_N; j++) {
-                    y.dat[i] += a.dat[i][j] * x.dat[j];
-                }
-            }
-        }
-
-        // Outer product
-        static void multiply(const vector_t & x, const vector_t & y, matrix_t & a)
-        {
-            for (uint8_t i=0; i<EKF_N; i++) {
-                for (uint8_t j=0; j<EKF_N; j++) {
-                    a.dat[i][j] = x.dat[i] * y.dat[j];
-                }
-            }
-        }
-
-        matrix_t _p;
-        vector_t _x;
-
-        bool _isUpdated;
-        uint32_t _lastPredictionMsec;
-        uint32_t _lastProcessNoiseUpdateMsec;
-        uint32_t _predictionIntervalMsec;
 
         axis3_t _gyroLatest;
 
